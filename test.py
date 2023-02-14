595882a618edb5e28e9fad4a9f93699c1e8b1aef
CoconutRemco <99071124@mydavinci.nl>
2023-02-14 11:36:10 +0100

    test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+def raadhetgetal():
+    import random
+    getal = random.randint(1, 100)
+    while True:
+        gok = input("Raad het getal: ")
+        if gok == "stop":
+            break
+        if int(gok) == getal:
+            print("Goed geraden!")
+            break
+        else:
+            print("Helaas, probeer het nog eens!")
